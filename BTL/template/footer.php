def9bcfd89de4ec7f63e8c4636bc8264e41b3384
thanhchi <thanhchi2kb@gmail.com>
2021-08-27 17:46:37 +0700

    up code giao dien lan1

---
+++ BTL/template/footer.php
@@ -0,0 +1,107 @@
+            <!-- footer -->
+            <footer class="footer container">
+                <div class="footer-main">
+                    <div class="row">
+                        <div class="col-6 col-sm-4 col-lg-2">
+                            <h3 class="title-list">CÓC HÓNG</h3>
+                            <ul class="list-link-ft">
+                                <li>
+                                    <a href="#">Cóc buôn</a>
+                                </li>
+                                <li>
+                                    <a href="#">Cóc quảng cáo</a>
+                                </li>
+                                <li>
+                                    <a href="#">Cóc tin</a>
+                                </li>
+                                <li>
+                                    <a href="#">Phóng sự nhà cóc</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-6 col-sm-4 col-lg-2">
+                            <h3 class="title-list">Cóc biết</h3>
+                            <ul class="list-link-ft">
+                                <li>
+                                    <a href="#">Cóc like</a>
+                                </li>
+                                <li>
+                                    <a href="#">Cóc thèm đọc</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-6 col-sm-4 col-lg-2">
+                            <h3 class="title-list">Cóc cần</h3>
+                            <ul class="list-link-ft">
+                                <li>
+                                    <a href="#">Cóc ăn chơi</a>
+                                </li>
+                                <li>
+                                    <a href="#">Cóc yêu</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-6 col-sm-4 col-lg-2">
+                            <h3 class="title-list">Cóc ngẫm</h3>
+                            <ul class="list-link-ft">
+                                <li>
+                                    <a href="#">Cóc mời</a>
+                                </li>
+                                <li>
+                                    <a href="#">Cóc mộng mơ</a>
+                                </li>
+                                <li>
+                                    <a href="#">Cóc nghĩ</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-6 col-sm-4 col-lg-2">
+                            <h3 class="title-list">Cóc gương</h3>
+                            <ul class="list-link-ft">
+                                <li>
+                                    <a href="#">Cóc cụ</a>
+                                </li>
+                                <li>
+                                    <a href="#">Cóc lên sóng</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-6 col-sm-4 col-lg-2">
+                            <h3 class="title-list">Cóc quay</h3>
+                            <ul class="list-link-ft">
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer-info">
+                    <div class="d-flex flex-column flex-sm-row">
+                        <a href="#" class="logo-1"><img
+                                src="https://fptcocdoc.todo.vn/upload/config/original/quy-tu-thien-logo-small-1600229454.png"
+                                alt="Trang chủ"></a>
+                        <div class="info">
+                            <h4>Báo Cóc Đọc và Những người bạn </h4>
+                            <p>
+                                Tổ chức Giáo dục FPT<br>
+                                Khu Công nghệ cao, Hòa Lạc, Thạch Thất, Hà Nội<br>
+                                Email: cocdoc@fe.edu.vn
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj"
+                crossorigin="anonymous"></script>
+            <script src="./bootstrap-5.1.0-dist/js/bootstrap.min.js"
+                integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/"
+                crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"
+                integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp"
+                crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js"
+                integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/"
+                crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ BTL/template/header.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container-sm">
+        <!-- header-main -->
+        <div class="header-main">
+            <div class="row align-items-start">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="logo">
+                            <a href="./index.php" class="logo-1"><img
+                                    src="https://fptcocdoc.todo.vn/upload/config/original/quy-tu-thien-logo-small-1600229454.png"
+                                    alt="Trang chủ"></a>
+                            <a href="./index.php" class="logo-2"><img
+                                    src="https://fptcocdoc.todo.vn/upload/config/original/quy-tu-thien-logo-1600229454.png"
+                                    alt="Trang chủ"></a>
+                        </div>
+                    </div>
+                    <div class="col-md-5 ms-auto">
+                        <form class="search">
+                            <input type="text" placeholder="Nhập để tìm kiếm" name="q" value="">
+                            <button><img src="https://ihe.fpt.edu.vn/template/images/ic_search.png"
+                                    alt="Tìm kiếm"></button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- header meunu -->
+        <div class="header-menu">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#"><img
+                                        src="https://ihe.fpt.edu.vn/template/images/menu-icon/icon_home.png"
+                                        alt="Trang chủ"></a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <img src="https://ihe.fpt.edu.vn/upload/original/cxampptmpphpeae4tmp-1599468902.png"
+                                    alt="Cóc hóng">
+                                <a class="nav-link " href="#" id="navbarDropdownMenuLink" role="button"
+                                    data-bs-toggle="dropdown" aria-expanded="false">
+                                    Cóc hóng
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <li><a class="dropdown-item" href="#">Cóc buôn</a></li>
+                                    <li><a class="dropdown-item" href="#">Cóc quảng cáo</a></li>
+                                    <li><a class="dropdown-item" href="#">Cóc tin</a></li>
+                                    <li><a class="dropdown-item" href="#">phóng sự nhà cóc</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <img src="https://ihe.fpt.edu.vn/upload/original/coc-biet-icons-1600310597.png"
+                                    alt="Cóc biết">
+                                <a class="nav-link " href="#" id="navbarDropdownMenuLink" role="button"
+                                    data-bs-toggle="dropdown" aria-expanded="false">
+                                    Cóc biết
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <li><a class="dropdown-item" href="#">Cóc thèm đọc</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <img src="https://ihe.fpt.edu.vn/upload/original/coc-can-icons-1601546584.png"
+                                    alt="Cóc cần">
+                                <a class="nav-link " href="#" id="navbarDropdownMenuLink" role="button"
+                                    data-bs-toggle="dropdown" aria-expanded="false">
+                                    Cóc cần
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <li><a class="dropdown-item" href="#">Cóc ăn chơi</a></li>
+                                    <li><a class="dropdown-item" href="#">Cóc yêu</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <img src="https://ihe.fpt.edu.vn/upload/original/coc-ngam-icons-1600313417.png"
+                                    alt="Cóc ngẫm">
+                                <a class="nav-link " href="#" id="navbarDropdownMenuLink" role="button"
+                                    data-bs-toggle="dropdown" aria-expanded="false">
+                                    Cóc ngẫm
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <li><a class="dropdown-item" href="#">Cóc mời</a></li>
+                                    <li><a class="dropdown-item" href="#">Cóc mộng mơ</a></li>
+                                    <li><a class="dropdown-item" href="#">Cóc nghĩ</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <img src="https://ihe.fpt.edu.vn/upload/original/coc-guong-icons-1600313438.png"
+                                    alt="Cóc gương">
+                                <a class="nav-link " href="#" id="navbarDropdownMenuLink" role="button"
+                                    data-bs-toggle="dropdown" aria-expanded="false">
+                                    Cóc gương
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <li><a class="dropdown-item" href="#">Cóc cày</a></li>
+                                    <li><a class="dropdown-item" href="#">Cóc cụ</a></li>
+                                    <li><a class="dropdown-item" href="#">Cóc lên sóng</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#"><img
+                                        src="https://ihe.fpt.edu.vn/upload/original/coc-quay-icons-1599753896.png"
+                                        alt="Cóc quay"></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+
+        <!-- banner-sldier-home container -->
+        <div class="banner-sldier-home">
+            <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active" data-bs-interval="10000">
+                        <img src="https://ihe.fpt.edu.vn/upload/feature/original/bao-coc-doc-thang-62021-1625214703.jpg"
+                            class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item" data-bs-interval="2000">
+                        <img src="https://ihe.fpt.edu.vn/upload/feature/original/banner-coc-doc-thang-72021-1628073408.jpg"
+                            class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://ihe.fpt.edu.vn/upload/feature/original/banner-coc-doc-thang-72021-1628073408.jpg"
+                            class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://ihe.fpt.edu.vn/upload/feature/original/bao-coc-doc-thang-62021-1625214703.jpg"
+                            class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://ihe.fpt.edu.vn/upload/feature/original/bao-coc-doc-thang-52021-1621484446.jpg"
+                            class="d-block w-100" alt="...">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval"
+                    data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval"
+                    data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                </button>
+            </div>
+        </div>
